ce8029e7350636d72e07f60efc14f41d48e5fbb9
Arnav Gupta <dev@championswimmer.in>
2015-09-06 05:25:06 -0400

    first commit

---
+++ Assets/JSONReader.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+using System.IO;
+
+public class JSONReader : MonoBehaviour {
+	string inp_ln = "";
+
+	// Use this for initialization
+	void Start () {
+	
+		string mailJson = readTextFile ("Assets/mail.json");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	string readTextFile(string file_path)
+	{
+		StreamReader inp_stm = new StreamReader(file_path);
+		
+		while(!inp_stm.EndOfStream)
+		{
+			inp_ln = inp_stm.ReadLine ();
+			// Do Something with the input. 
+		}
+		
+		inp_stm.Close( );  
+		return inp_ln;
+	}
+}
